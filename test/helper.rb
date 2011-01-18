5e785a39cd41a6b79be0843bb83bdba218d115ab
Ryan Sonnek <ryan@socialcast.com>
2011-01-18 14:07:08 -0600

    fix running tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,15 @@
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 require 'test/unit'
+require 'shoulda'
+require 'timecop'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
